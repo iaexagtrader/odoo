8db8618c9c6b75f699e079d7012a574934aa5346
iaexagtrader <iaexagtrader@gmail.com>
2026-05-04 04:08:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,23 @@
 
 set -e
 
-echo Waiting for database...
+echo "Esperando a PostgreSQL..."
 
-while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done; 
+# Railway usa PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDATABASE
+while ! nc -z "$PGHOST" "$PGPORT" 2>&1; do
+  sleep 1
+done
 
-echo Database is now available
+echo "PostgreSQL está listo."
+
+# Si PORT no existe, usar 8069
+ODOO_PORT=${PORT:-8069}
 
 exec odoo \
-    --http-port="${PORT}" \
-    --init=all \
-    --without-demo=True \
+    --http-port="$ODOO_PORT" \
     --proxy-mode \
-    --db_host="${ODOO_DATABASE_HOST}" \
-    --db_port="${ODOO_DATABASE_PORT}" \
-    --db_user="${ODOO_DATABASE_USER}" \
-    --db_password="${ODOO_DATABASE_PASSWORD}" \
-    --database="${ODOO_DATABASE_NAME}" \
-    --smtp="${ODOO_SMTP_HOST}" \
-    --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
-    --smtp-user="${ODOO_SMTP_USER}" \
-    --smtp-password="${ODOO_SMTP_PASSWORD}" \
-    --email-from="${ODOO_EMAIL_FROM}" 2>&1
+    --db_host="$PGHOST" \
+    --db_port="$PGPORT" \
+    --db_user="$PGUSER" \
+    --db_password="$PGPASSWORD" \
+    --database="$PGDATABASE" 2>&1
