7014458ecd707707c9aebbdff89b745a4b3e7275
iaexagtrader <iaexagtrader@gmail.com>
2026-05-04 04:13:21 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,12 @@ set -e
 
 echo "Esperando a PostgreSQL..."
 
-# Railway usa PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDATABASE
 while ! nc -z "$PGHOST" "$PGPORT" 2>&1; do
   sleep 1
 done
 
 echo "PostgreSQL está listo."
 
-# Si PORT no existe, usar 8069
 ODOO_PORT=${PORT:-8069}
 
 exec odoo \
